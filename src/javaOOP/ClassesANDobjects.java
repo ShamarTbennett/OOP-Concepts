fa1ce84bfa259061e0c2fce43dc3b0a078a41093
Shamar Bennett <bennettshamar96@gmail.com>
2025-12-09 19:15:03 -0500

    added somce code to the classANDobject class

--- src/javaOOP/ClassesANDobjects.java
+++ src/javaOOP/ClassesANDobjects.java
@@ -2,4 +2,24 @@ package javaOOP;
 
 public class ClassesANDobjects {
     
+    // state or field
+    private int gear = 5;
+
+    // behavior or method
+    public void braking() {
+        System.out.println("Working of Braking");
+    }
+
+    
+}
+
+public class Vains{
+
+    public static void main(String[] args) {
+        // create object
+    ClassesANDobjects sportsBicycle = new ClassesANDobjects();
+
+    // access field and method
+    sportsBicycle.braking();
+    }
 }
