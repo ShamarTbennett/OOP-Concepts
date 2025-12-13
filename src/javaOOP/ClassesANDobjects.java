d2b1ea83b014093668fe5f4b5b270df0e9b004a8
Shamar Bennett <bennettshamar96@gmail.com>
2025-12-13 18:23:11 -0500

    added some code

--- src/javaOOP/ClassesANDobjects.java
+++ src/javaOOP/ClassesANDobjects.java
@@ -2,25 +2,29 @@ package javaOOP;
 
 public class ClassesANDobjects {
     
-    // state or field
+    // State (field): represents the gear of the bike
     private int gear = 5;
 
-    // behavior or method
+    // Behavior (method): simulates braking
     public void braking() {
         System.out.println("Working of Braking");
     }
 
-    // behavior or method
+    // Another method (note: there's a typo in "brakinsg" - it should be "braking")
     public void brakinsg() {
         System.out.println("Working of Braking");
     }
 
-     //object creation
-     public static void main(String[] args) {
+    // Main method: creates an object and demonstrates usage
+    public static void main(String[] args) {
+        // Creating an object (instance) of the class
         ClassesANDobjects bike = new ClassesANDobjects();
+        
+        // Calling a method on the object
         bike.braking();
+        
+        // Accessing a field of the object
         System.out.println("Gear of Bike: " + bike.gear);
-    
     }
 }
 
