d1eea6273cae5f3bc56fcaa84f6f795cffa8f309
Shamar Bennett <bennettshamar96@gmail.com>
2025-12-14 21:01:24 -0500

    Added the date class in the ClassesAndOcject folder

--- src/javaOOP/objectsAndClasses/Date.java
+++ src/javaOOP/objectsAndClasses/Date.java
@@ -26,4 +26,29 @@ public class Date {
         month = date.month;
         year = date.year;
     }
+
+    // Mutators      
+    public void setDay(int day){
+        this.day = day;
+    }
+    public void setMonth(int month){
+        this.month = month;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    // Accessors      
+    public int getDay() {
+        return day;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public int getYear() {
+        return year;
+    }
+
+
+   
 }
