e47db896d6512f5ae597b6e5fd2147d5b3457fca
Shamar Bennett <bennettshamar96@gmail.com>
2025-12-16 17:55:03 -0500

    added code to the transponderUnit File

--- src/javaOOP/informationhiding/TransponderUnit.java
+++ src/javaOOP/informationhiding/TransponderUnit.java
@@ -2,4 +2,43 @@ package javaOOP.informationhiding;
 
 public class TransponderUnit {
     
+    private int totalUnits;
+
+    //primary constructor
+    public TransponderUnit(){
+        totalUnits = 0;
+    }
+
+    //Default Constructor
+    public TransponderUnit(int totalUnits){
+        this.totalUnits = totalUnits;
+    }
+
+    //Copy constructor
+    public TransponderUnit(TransponderUnit unit){
+        totalUnits = unit.totalUnits;
+    }
+
+
+    public void send(){
+
+    }
+
+    public void recieve(){
+
+    }
+
+    public void display(){
+
+    }
+
+    public void setTotalunits(int units){
+        totalUnits = units;
+    }
+
+    public int getTotalUnits(){
+        return totalUnits;
+    }
+
+
 }
